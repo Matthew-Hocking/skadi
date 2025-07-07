0a66d40b73ab668b5d06b8626c85325f6e24e12c
Matthew-Hocking <matthocking2439@gmail.com>
2025-07-07 15:45:29 +1200

    add new colors + animations, dark theme variables, new fonts

--- src/components/landing-page/Nav.tsx
+++ src/components/landing-page/Nav.tsx
@@ -1,23 +1,35 @@
 type NavProps = {
   onSignInClick: () => void;
   onSignUpClick: () => void;
-}
+};
 
 const Nav = ({ onSignInClick, onSignUpClick }: NavProps) => {
   return (
     <header>
       <nav class="container flex items-center justify-between py-4">
-        <a href="#top" class="text-xl font-bold text-azul">Skadi</a>
+        <a
+          href="#top"
+          class="text-5xl font-light text-azul dark:text-mint-cream"
+        >
+          <span class="underline-hover josefin-sans-display">Skadi</span>
+        </a>
 
-        <ul class="hidden md:flex gap-6 whitespace-nowrap" role="navigation">
+        <ul
+          class="hidden md:flex gap-6 whitespace-nowrap"
+          role="navigation"
+        ></ul>
 
-        </ul>
-
-        <div class="flex gap-4 whitespace-nowrap">
-          <button class="sign-in" onClick={onSignInClick}>
+        <div class="flex gap-4 whitespace-nowrap dark:text-mint-cream">
+          <button
+            class="text-azul hover:text-azul-hover transition underline-hover py-4 px-10 dark:text-mint-cream dark:hover:text-mint-cream"
+            onClick={onSignInClick}
+          >
             Sign in
           </button>
-          <button class="sign-up" onClick={onSignUpClick}>
+          <button
+            class="text-white py-4 px-10 bg-azul hover:bg-azul-hover transition duration-[0.3s] ease-in-out dark:raisin-black"
+            onClick={onSignUpClick}
+          >
             Sign up
           </button>
         </div>
