8900857df29392da4d9abacf877b92df33bbc372
Matthew-Hocking <matthocking2439@gmail.com>
2025-07-08 12:20:44 +1200

    don't let users view lading page - redirect to dashboard

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,9 +1,18 @@
 import { useState } from "preact/hooks";
+import { useLocation } from "preact-iso";
 import { AuthModal, Nav } from "../components/landing-page";
+import { useAuth } from "../hooks/useAuth";
 
 const LandingPage = () => {
   const [showAuthModal, setShowAuthModal] = useState(false);
   const [authMode, setAuthMode] = useState<'signin' | 'signup'>('signin');
+
+  const { user } = useAuth();
+  const { route } = useLocation()
+
+  if (user) {
+    route('/dashboard')
+  }
 
   return (
     <main>
